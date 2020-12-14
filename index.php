27b71f7fb4b1e56007a09b0cffb9874a73dc6cf7
AbdielP <abdiel.pinzonc@gmail.com>
2020-12-14 03:59:51 -0500

    Se cambia formato de tarjetas para mostrar ofertas de productos en home page

--- index.php
+++ index.php
@@ -48,36 +48,101 @@
         <div class="col-md-3">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <h5 class="card-title">¡Bienvenido!</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Mercado de ofertas Online</h6>
+              <p class="card-text">Consulte o publique ofertas de productos en Ofertas Online. El mercado de ofertas al por mayor más éxitoso.</p>
+              <hr>
+              <!-- <a href="#" class="card-link">Card link</a> -->
+              <?php
+                if (isset($_SESSION["usuario_valido"])){
+                  print "<a href='ofertas/misofertas.php' class='card-link h6'><i class='fas fa-cash-register'></i> Mis ofertas</a>";
+                } else {
+                  print "<p align='center' class='m-0'>[ <a href='login.php' TARGET='_top'>Iniciar sesión</a> ]</p>";
+                }
+              ?>  
             </div>
           </div>
         </div>
 
         <div class="col-md-9">
-          <!-- Tarjeta de oferta más alta  -->
-          <div class="card">
+          <!-- Tarjeta de oferta más reciente  -->
+          <div class="card flex-md-row">
             <div class="card-body">
-              <h4 class="card-title">Mercado DS-7 <i class="fas fa-feather-alt"></i></h4>
-              <h6 class="card-subtitle mb-2 text-warning">Oferta más alta</h6>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ullam similique placeat vel perferendis architecto ducimus est voluptas obcaecati, 
-                temporibus amet itaque, harum laudantium, omnis voluptatibus veniam doloremque dolore distinctio quod autem repudiandae quam!.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <h4 class="card-title text-secondary"><i class="far fa-handshake"></i> Ofertas Online</h4>
+              <hr>
+              <div class="row">
+                <h6 class="card-subtitle mb-2 ml-3 text-info">Oferta más reciente</h6>
+                <h6 class="card-subtitle mb-2 ml-3 text-secondary">Producto</h6>
+              </div>
+              <p class="card-text">
+                Info de este producto. Descripción de esta oferta traida de la BD.
+              </p>
+              <button type="button" class="btn btn-sm btn-secondary">
+                Cantidad <span class="badge badge-pill badge-light">100</span>
+              </button>
+              <button type="button" class="btn btn-sm btn-secondary">
+                <span class="badge badge-pill badge-light">Lb</span>
+              </button>
             </div>
+
+            <div class="card-body"> <!-- INICIO DIV CARD-BODY -->
+
+              <div class="order-md-2 mb-4">
+                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                  <span class="text-muted">Precio</span>
+                  <span class="badge badge-secondary badge-pill">3</span>
+                </h4>
+                <ul class="list-group mb-3">
+                  <li class="list-group-item d-flex justify-content-between lh-condensed">
+                    <div>
+                      <h6 class="my-0">Product name</h6>
+                      <small class="text-muted">Brief description</small>
+                    </div>
+                    <span class="text-muted">$12</span>
+                  </li>
+                  
+                </ul>
+              </div>
+
+            </div> <!-- FINAL DIV CARD-BODY -->
           </div>
-          <!-- Tarjeta de oferta más baja  -->
-          <div class="card">
+
+          <!-- Tarjeta de oferta más alta y más baja  -->
+          <div class="card flex-md-row">
             <div class="card-body">
-              <h4 class="card-title">Mercado DS-7 <i class="fas fa-feather-alt"></i></h4>
-              <h6 class="card-subtitle mb-2 text-success">Oferta más Baja</h6>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ullam similique placeat vel perferendis architecto ducimus est voluptas obcaecati, 
-                temporibus amet itaque, harum laudantium, omnis voluptatibus veniam doloremque dolore distinctio quod autem repudiandae quam!.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <h4 class="card-title text-secondary"><i class="far fa-handshake"></i> Ofertas Online</h4>
+              <hr>
+              <div class="row">
+                <h6 class="card-subtitle mb-2 ml-3 text-warning">Oferta más Alta</h6>
+                <h6 class="card-subtitle mb-2 ml-3 text-secondary">Producto</h6>
+              </div>
+              <p class="card-text">
+                Info de este producto. Descripción de esta oferta traida de la BD.
+              </p>
+              <button type="button" class="btn btn-sm btn-secondary">
+                Cantidad <span class="badge badge-pill badge-light">100</span>
+              </button>
+              <button type="button" class="btn btn-sm btn-secondary">
+                <span class="badge badge-pill badge-light">Lb</span>
+              </button>
+            </div>
+
+            <div class="card-body">
+              <h4 class="card-title text-secondary"><i class="far fa-handshake"></i> Ofertas Online</h4>
+              <hr>
+              <div class="row">
+                <h6 class="card-subtitle mb-2 ml-3 text-success">Oferta más Baja</h6>
+                <h6 class="card-subtitle mb-2 ml-3 text-secondary">Producto</h6>
+              </div>
+              <p class="card-text">
+                Info de este producto. Descripción de esta oferta traida de la BD.
+              </p>
+              <button type="button" class="btn btn-sm btn-secondary">
+                Cantidad <span class="badge badge-pill badge-light">100</span>
+              </button>
+              <button type="button" class="btn btn-sm btn-secondary">
+                <span class="badge badge-pill badge-light">Lb</span>
+              </button>
             </div>
           </div>
           
